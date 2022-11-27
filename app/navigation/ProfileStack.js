7865ea33a75e68d593281092f2f6c19a486b1fd1
Jeanpy171 <jeanpyfuentes67@gmail.com>
2022-11-26 21:57:37 -0500

    Semana 2-25%

---
+++ app/navigation/ProfileStack.js
@@ -0,0 +1,25 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import React, { useContext } from 'react'
+import Profile from '../screens/Profile';
+import EditProfile from '../screens/EditProfile';
+import { AuthContext } from '../../src/context/AuthContext';
+import Splash from '../screens/Splash';
+
+
+const Stack = createStackNavigator();
+
+export const ProfileStack = () => {
+  const {user, logged, logout, isLogged,userInfo,avatar} = useContext(AuthContext)
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+    }}
+    >
+        <Stack.Screen name="ProfileScreen" component={Profile} />
+        <Stack.Screen name="EditProfileScreen" component={EditProfile} />
+    </Stack.Navigator>
+  );
+}
+
+export default ProfileStack
